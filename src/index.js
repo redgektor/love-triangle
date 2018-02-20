22b96553d853091351fa6d059c580a341a6108b7
redgektor <redgektor11@gmail.com>
2018-02-20 23:40:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+    for (var i = 0; i < preferences.length; i++) {
+        for (var j = 0; j < preferences.length; j++) {
+            if (i == preferences[j] - 1) {
+                for (var k = 0; k < preferences.length; k++) {
+                    if (preferences[k] - 1 == j) {
+                        if (preferences[i] - 1  == k) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+    } return Math.floor(count/3);
 };
